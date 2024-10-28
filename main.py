c8cc5fb7b045436208627ad428f6b6418c63730a
FilinMen <fiilinkadabra@tutamail.com>
2024-10-28 17:05:58 +0300

    good

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ input_data = open('input.txt','r')
 data = input_data.read()
 a = int(data)
 b = 1
-if a > 0:
+if a > 1:
     for i in range(1,a+1):
         b = b * i
 else:
     b = 1
 b = str(b)
-output_data = open("ouyput.txt","w")
+output_data = open("output.txt","w")
 output_data.write(b)
 input_data.close()
 output_data.close()
